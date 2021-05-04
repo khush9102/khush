e674c4fd6e215bce3483efc3fcb78ab344d83e65
khush thakkar <khush.thakkar@contcentric.com>
2021-05-04 20:48:24 +0530

    myfiles

---
+++ DefaultValue.java
@@ -0,0 +1,23 @@
+public class DefaultValue
+{
+	static boolean a;
+   	static byte b;
+	static String c;
+   	static short d;
+   	static int e;
+   	static long f;
+	static float g;
+	static double h;
+   	
+	public static void main(String[] args)
+	{
+      		System.out.println("Default value of boolean: " + a);
+      		System.out.println("Default value of byte: " + b);
+      		System.out.println("Default value of char: " + c);
+      		System.out.println("Default value of short: " + d);
+      		System.out.println("Default value of int: " + e);
+      		System.out.println("Default value of long: " + f);
+      		System.out.println("Default value of float: " + g);
+      		System.out.println("Default value of double: " + h);
+   	}
+}
